4610773e21ec76a75e4d63e473adc8863e40f68a
cgulli <caitlin.gulliford@gmail.com>
2017-05-03 14:25:09 +0100

    [Jess/Caitlin] Initial commit for Front End microservice with react initialiased and mock json file

---
+++ src/components/app.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = { name: ''};
+  }
+
+  componentDidMount() {
+   this.httpGetAsync("http://localhost:3000/data.json");
+  }
+
+  setName(response) {
+    var parsedResponse =JSON.parse(response);
+    console.log(parsedResponse["name"].toString());
+    this.setState({name: parsedResponse["name"].toString()});
+  }
+
+  httpGetAsync(theURL){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        this.setName(xmlHttp.responseText);
+    }.bind(this);
+    xmlHttp.open( "GET", theURL, true );
+    xmlHttp.send( null );
+  }
+
+  render() {
+  return (
+    <div>
+      {this.state.name}
+    </div>
+    );
+  }
+}
