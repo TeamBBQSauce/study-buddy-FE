a3c18f40b2376f6a6168bde81e6bbec62a187691
Jess Astbury <jess.astbury@hotmail.com>
2017-05-08 10:24:28 +0100

    Fixed linter warning

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class App extends Component {
-  constructor(){
-    super();
-    this.state = { name: ''};
+  constructor () {
+    super()
+    this.state = { name: ''}
   }
 
-  componentDidMount() {
-   this.httpGetAsync("http://localhost:3000/data.json");
+  componentDidMount () {
+    this.httpGetAsync('http://localhost:3000/data.json')
   }
 
-  setName(response) {
-    var parsedResponse =JSON.parse(response);
-    console.log(parsedResponse["name"].toString());
-    this.setState({name: parsedResponse["name"].toString()});
+  setName (response) {
+    var parsedResponse = JSON.parse(response)
+    this.setState({name: parsedResponse['name'].toString()})
   }
 
-  httpGetAsync(theURL){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-        this.setName(xmlHttp.responseText);
-    }.bind(this);
-    xmlHttp.open( "GET", theURL, true );
-    xmlHttp.send( null );
+  httpGetAsync (theURL) {
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) { this.setName(xmlHttp.responseText) }
+    }.bind(this)
+    xmlHttp.open('GET', theURL, true)
+    xmlHttp.send(null)
   }
 
-  render() {
-  return (
+  render () {
+    return (
     <div>
       {this.state.name}
     </div>
-    );
+    )
   }
 }
